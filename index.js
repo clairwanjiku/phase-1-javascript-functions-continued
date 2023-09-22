501ee72131b9e0c352fe529ff0518b5d1a6715e7
Clair Wanjiku <clair.karanja@student.moringaschool.com>
2023-09-22 11:37:42 +0300

    Completed assignment

--- index.js
+++ index.js
@@ -1,1 +1,19 @@
 // code your solution here
+ function  SaturdayFunday(activity="roller-skate"){
+    
+    return `This Saturday, I want to ${activity}!`
+ }
+
+
+
+function  mondayWork(activity="go to the office") {
+    return `This Monday, I will ${activity}.`
+}
+
+ 
+
+function wrapAdjective(string="*") {
+    return function(parameter="special") {
+        return `You are ${string}${parameter}${string}!`
+    }
+}
